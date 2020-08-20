8ba8a2e9c37457d7867b1529ad49cfba311a6bb6
pratikshad16 <dhagepratiksha16@gmail.com>
2020-08-20 16:32:09 +0530

    Added: Functionality for checking options by player

--- Snake_Ladder.sh
+++ Snake_Ladder.sh
@@ -1,12 +1,40 @@
 #!/bin/bash -x
 #constant
-START_POSITION = 0
+START_POSITION=0
+NO_PLAY=0
+SNAKE=1
+LADDER=2
 
 #variable
 dice=0
+option=0
+position=$START_POSITION
 
 rollDie() {
 	dice=$(( RANDOM % 6 + 1 ))
+	option=$(( RANDOM % 3 + 1 ))
 }
-rollDie
-echo $dice
+checkOption() {
+	rollDie
+	case $option in
+		1)
+			echo "It is a snake option"
+			result=$(($position-$dice))
+			if [ $result -gt 0 ]
+			then
+				position=$(($position-$dice))
+			else
+				position=$position
+			fi
+			;;
+		2)
+			echo "It is a ladder option"
+			position=$(($position+$dice))
+			;;
+		3)
+			echo "It is no play option"
+			position=$position
+			;;
+		esac
+}
+checkOption
