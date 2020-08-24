b8b1970096e289ec57ebc8d7d1d2454cb0795f9c
pratikshad16 <dhagepratiksha16@gmail.com>
2020-08-25 00:59:05 +0530

    Added: Functionality for game with two players

--- Snake_Ladder.sh
+++ Snake_Ladder.sh
@@ -11,41 +11,38 @@ dice=0
 option=0
 position=$START_POSITION
 numberOfTimesDiceRolled=0
+player1=0
+player2=0
 count=0
+turn=0
 
 declare -a array
 
+#function for rolling a die
 rollDie() {
 	dice=$(( RANDOM % 6 + 1 ))
 	option=$(( RANDOM % 3 + 1 ))
-	(( numberOfTimesDiceRolled++ ))
+	numberOfTimesDiceRolled=$(( $numberOfTimesDiceRolled + 1 ))
 }
+
+#function for checking options
 checkOption() {
 	rollDie
 	case $option in
 		1)
-			echo "It is a snake option"
-			result=$(($position-$dice))
-			if [ $result -gt 0 ]
-			then
-				position=$(($position-$dice))
-			else
-				position=$position
-			fi
+			position=$(($position-$dice))
 			;;
 		2)
-			echo "It is a ladder option"
 			position=$(($position+$dice))
 			;;
 		3)
-			echo "It is no play option"
 			position=$position
 			;;
 		esac
 }
+
+#function for winning condition till exact 100 position
 winningPosition() {
-	while [ $position -ne $FINAL_POSITION ] || [ $position -gt $FINAL_POSITION ]
-	do
 		checkOption
 		if [ $position -lt $START_POSITION ]
 		then
@@ -56,14 +53,55 @@ winningPosition() {
 		fi
 		array[$count]=$position
 		count=$(($count + 1))
+}
+
+#function for current position
+currentPosition() {
+	position=$1
+	winningPosition
+}
+
+#function for checking win
+checkWin() {
+	while [ $position -ne $FINAL_POSITION ] || [ $position -gt $FINAL_POSITION ]
+	do
+		if [ $turn -eq 0 ]
+		then
+			turn=1
+			currentPosition $player1
+			player1=$position
+		else
+			turn=0
+			currentPosition $player2
+			player2=$position
+		fi
+	done
+	turn=0
+}
+
+#function for printing position after every die
+positionAfterEveryDie() {
+	checkWin
+	for (( boardPosition=0 ; boardPosition<$numberOfTimesDiceRolled ; boardPosition++ ))
+	do
+		if [ $turn -eq 0 ]
+		then
+			turn=1
+			echo "Turn of Dice $(($boardPosition +1)) : Player1 : Position ${array[$boardPosition]}"
+		else
+			turn=0
+			echo "Turn of Dice $(($boardPosition +1)) : Player2 : Position ${array[$boardPosition]}"
+		fi
 	done
 }
 
-positionAfterEveryDie() {
-	winningPosition
-	for (( boardPosition=0 ; boardPosition<$numberOfTimesDiceRolled ; boardPosition++ ))
-	do
-		echo "Turn of Dice $(($boardPosition +1)) : Position ${array[$boardPosition]}"
-	done
-}
 positionAfterEveryDie
+
+#check who wins
+if [ $player1 -gt $player2 ]
+then
+	echo "Player1 wins..."
+else
+	echo "Player2 wins..."
+fi
+echo "Total number of times dice Rolled: $boardPosition"
